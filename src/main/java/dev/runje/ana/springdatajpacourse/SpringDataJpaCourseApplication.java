2cc1f053b037b430f30f3e5f55d9b3245c414cd4
ana.runje <ana.runje@clover.com>
2023-11-20 12:30:11 +0000

    initial commit

---
+++ src/main/java/dev/runje/ana/springdatajpacourse/SpringDataJpaCourseApplication.java
@@ -0,0 +1,13 @@
+package dev.runje.ana.springdatajpacourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataJpaCourseApplication.class, args);
+    }
+
+}
